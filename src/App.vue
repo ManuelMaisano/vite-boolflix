7b31a42fe7bd16b969671f920b033eae560aebba
Manuel Maisano <manumaisano@gmail.com>
2024-04-18 18:51:43 +0200

    headercomponent

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,53 @@
 <template>
+  <div>
+    <HeaderComponent />
  
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
 import { store } from './store';
-  export default {
-    name: 'App',
-data() {
-  return {
-    store,
-  };
-},
-methods: {
-  getMovies() {
-    axios.get(this.store.apiUrl + this.store.endPoint.movie, this.store.options)
-      .then((res) => {
-        console.log(res.data.results);
-      });
-     
+import HeaderComponent from './components/HeaderComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    
   },
-  getSeries() {
-    axios.get(this.store.apiUrl + this.store.endPoint.series, this.store.options)
-      .then((res) => {
-        console.log(res.data.results);
-      });
-     
+  data() {
+    return {
+      store,
+    };
   },
-},
-created() {
-  this.getMovies();
-  this.getSeries();
-},
+  methods: {
+    getMovies() {
+      axios.get(this.store.apiUrl + this.store.endPoint.movie, this.store.options)
+        .then((res) => {
+          console.log(res.data.results);
+        });
+
+    },
+    getSeries() {
+      axios.get(this.store.apiUrl + this.store.endPoint.series, this.store.options)
+        .then((res) => {
+          console.log(res.data.results);
+        });
+
+    },
+  },
+  created() {
+    this.getMovies();
+    this.getSeries();
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+
+
+
+
 
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,59 @@
 <template>
-    <div>
+    <div id="header">
+        <div class="d-flex align-items-center">
+            <img class="img-fluid" src="/public/images/netflix.jpg" alt="LogoNetflix">
+            <ul class="d-flex">
+                <li><a href="">Home</a></li>
+                <li><a href="#">Serie TV</a></li>
+                <li><a href="#">Film</a></li>
+                <li><a href="#">Nuovi e popolari</a></li>
+                <li><a href="#">La mia lista</a></li>
+                <li><a href="#">Sfoglia per lingua</a></li>
+            </ul>
+        </div>
 
     </div>
 </template>
 
 <script>
-    export default {
-        
+export default {
+    name: 'HeaderComponent',
+    data() {
+        return {
+
+        }
     }
+
+}
 </script>
 
 <style lang="scss" scoped>
+#header {
+    height: 150px;
+    background-color: #010101;
 
+}
+
+img {
+    height: 150px;
+    width: 150px;
+
+}
+
+li {
+    list-style: none;
+    padding: 20px;
+}
+
+
+a {
+    text-decoration: none;
+
+    color: white;
+    font-weight: bold;
+
+    &:hover {
+        color: lightgray;
+    }
+}
 </style>
